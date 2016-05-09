5215b15bf0d3df33d60080cbbcb6b0b109852657
xiaohongxiedaima <redfishinaction@yahoo.com>
2016-05-10 01:26:23 +0800

    [add] 通过zookeeper原生api创建zookeeper连接

---
+++ ondemo-zookeeper/src/main/java/com/xiaohongxiedaima/demo/zookeeper/SimpleWatcher.java
@@ -0,0 +1,37 @@
+package com.xiaohongxiedaima.demo.zookeeper;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author xiaohongxiedaima
+ * @version 16/5/10
+ * @E-mail redfishinaction@yahoo.com
+ */
+public class SimpleWatcher implements Watcher {
+
+    private static final Logger logger = LoggerFactory.getLogger(SimpleWatcher.class);
+
+    private CountDownLatch countDownLatch = new CountDownLatch(1);
+
+    public boolean await() {
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+
+        }
+        return true;
+    }
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+        logger.info("watchedEvent: {}", watchedEvent);
+        if (Event.KeeperState.SyncConnected == watchedEvent.getState()) {
+            countDownLatch.countDown();
+        }
+    }
+}
---
+++ ondemo-zookeeper/src/main/java/com/xiaohongxiedaima/demo/zookeeper/ZooKeeperClient.java
@@ -0,0 +1,46 @@
+package com.xiaohongxiedaima.demo.zookeeper;
+
+import org.apache.zookeeper.ZooKeeper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author xiaohongxiedaima
+ * @version 16/5/10
+ * @E-mail redfishinaction@yahoo.com
+ */
+public class ZooKeeperClient{
+
+    private static final Logger logger = LoggerFactory.getLogger(ZooKeeperClient.class);
+
+    /**
+     *
+     * @param connectionString ip:port,ip:port/node/node
+     * @param sessionTimeout
+     * @return
+     */
+    public static ZooKeeper getInstance(String connectionString, int sessionTimeout) {
+        ZooKeeper zooKeeper = null;
+        try {
+            SimpleWatcher watcher = new SimpleWatcher();
+            zooKeeper = new ZooKeeper(connectionString, sessionTimeout, watcher);
+            logger.info("zookeeper connection state: {}", zooKeeper.getState());
+            watcher.await();
+            logger.info("zookeeper connection state: {}", zooKeeper.getState());
+            logger.info("zookeeper session establelished" );
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        } finally {
+            return zooKeeper;
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String connectionString = "127.0.0.1:2181/zookeeper";
+        int sessionTimeout = 5000;
+        ZooKeeper zooKeeper = ZooKeeperClient.getInstance(connectionString, sessionTimeout);
+        logger.info(zooKeeper.toString());
+        zooKeeper.close();
+    }
+
+}
