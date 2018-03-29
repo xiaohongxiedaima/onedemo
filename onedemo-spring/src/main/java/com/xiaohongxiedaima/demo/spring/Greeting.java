b073463483929ac971d2aead38b18636597afc78
xiaohongxiedaima <redfishinaction@yahoo.com>
2018-03-29 11:26:52 +0800

    delete thrift demo project

---
+++ onedemo-spring/src/main/java/com/xiaohongxiedaima/demo/spring/Greeting.java
@@ -0,0 +1,23 @@
+package com.xiaohongxiedaima.demo.spring;
+
+/**
+ * Created by liusheng on 18-3-13.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
